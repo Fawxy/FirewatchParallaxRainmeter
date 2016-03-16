727a50c5bcb72abc27f4d4c4a13344d9ed125994
Fawxy <byatt.chris@gmail.com>
2016-03-16 22:39:03 +0000

    Tried to increase effiecincy of matrix generation

--- @Resources/scripts/Matrix.lua
+++ @Resources/scripts/Matrix.lua
@@ -1,3 +1,5 @@
+string = ""
+
 function Initialize()
   default = {{1, 0, 0, 0, 0}, {0, 1, 0, 0, 0}, {0, 0, 1, 0, 0}, {0, 0, 0, 1, 0}, {0, 0, 0, 0, 1}}
   dawn = {{0.5771806415306519, -0.05207895481521974, 0.32602478689264475, 0, 0}, {0.3462206377905974, 0.48313224358999163, -0.23516468548063502, 0, 0}, {-0.37340127932124934, 0.11894671122522803, 0.45913989858799026, 0, 0}, {0, 0, 0, 1, 0}, {0.028575, 0.028575, 0.028575, 0, 1}}
@@ -14,16 +16,12 @@ function CalculateMatrix(startMatrix, endMatrix, animationLength, count)
       end
    end
 
-   local string = ""
    for i=1, 5 do
-     for j=1, 5 do
-       if j < 5 then
-         string = string..(startMatrix[i][j] - (difference[i][j] * count))..';'
-       else
-         string = string..(startMatrix[i][j] - (difference[i][j] * count))
-         SKIN:Bang('!SetVariable MatrixRow'..i..' '..string)
-         string = ""
-       end
+     string = (startMatrix[i][1] - (difference[i][1] * count))..';'
+     for j=2, 4 do
+       string = string..(startMatrix[i][j] - (difference[i][j] * count))..';'
      end
+     string = string..(startMatrix[i][5] - (difference[i][5] * count))
+     SKIN:Bang('!SetOptionGroup ColourMatrixGroup ColorMatrix'..i..' '..string)
    end
 end
